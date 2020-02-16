d2a0f2c4f4aa37b0151a96c8329f2a827fb65cd5
ywemay <srv023@ywemay.com>
2020-02-16 13:35:38 +0800

    Initial commit

--- mock/index.js
+++ mock/index.js
@@ -3,10 +3,12 @@ import { param2Obj } from '../src/utils'
 
 import user from './user'
 import table from './table'
+import country from './country'
 
 const mocks = [
   ...user,
-  ...table
+  ...table,
+  ...country
 ]
 
 // for front mock
