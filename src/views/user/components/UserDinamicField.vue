682e10694d48cf0902c3b8b626137ef5ae94c5d4
Dorin Chibuc <srv023@ywemay.com>
2020-03-02 11:14:04 +0800

    Working on UserDinamicField

---
+++ src/views/user/components/UserDinamicField.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-form-item class="user-dinamic-field">
+    <div v-if="state === 'search'" class="user-edit">
+       v-model="searchName"
+    </div>
+    <div v-if="state === 'edit'" class="user-edit">
+    </div>
+    <div v-elseif="state === 'create'" class="user-create">
+    </div>
+    <div v-else class="user-view">
+    </div>
+  </el-form-item>
+</template>
+
+<script>
+
+export default {
+  name: 'UserDinamicField',
+  props: {
+    user: {
+      type: Object,
+      default: undefined
+    }
+  }
+  data() {
+    return {
+      state: 'search',
+      searchName: ''
+    }
+  },
+  mounted() {
+    if (this.user !== undefined) {
+      this.state = 'view'
+    }
+  }
+}
+</script>
